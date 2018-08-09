b9aa606ed1e2391de44c261109296c48a0cca6e2
katzaz10 <katzaz10@gmail.com>
2018-08-09 10:43:36 -0400

    passed all tests

--- HackerRankSolutions/StringManipulation/MakingAnagrams/solution.py
+++ HackerRankSolutions/StringManipulation/MakingAnagrams/solution.py
@@ -1,45 +1,29 @@
-from Queue import Queue
-
-def listtoqueue(arr):
-    '''Converts an list into a queue'''
-    que = Queue()
-    for i in arr:
-        que.put(i)
-    return que
-
-
-def makeAnagram(a, b):
-    lista = list(a)
-    listb = list(b)
-    lista.sort()
-    listb.sort()
-    aVals = listtoqueue(lista)
-    bVals = listtoqueue(listb)
+def makeAnagram(a,b):
+    list_a = list(a)
+    list_b = list(b)
+    list_a.sort()
+    list_b.sort()
+    a_len = len(list_a)
+    b_len = len(list_b)
+    a_ind = 0
+    b_ind = 0
     delete = 0
-    cur_a = aVals.get()
-    cur_b = bVals.get()
 
     while True:
-        if aVals.empty():
-            while not bVals.empty():
-                bVals.get()
-                delete += 1
-            return delete
-        if bVals.empty():
-            while not aVals.empty():
-                aVals.get()
-                delete += 1
-            return delete
+        if a_ind == a_len:
+            return delete + b_len - b_ind
+        if b_ind == b_len:
+            return delete + a_len - a_ind
+
+        cur_a = list_a[a_ind]
+        cur_b = list_b[b_ind]
+
         if cur_a == cur_b:
-            if not aVals.empty():
-                cur_a = aVals.get()
-            if not bVals.empty():
-                cur_b = bVals.get()
+            a_ind += 1
+            b_ind += 1
         elif cur_a < cur_b:
-            while not aVals.empty() and cur_a < cur_b:
-                delete += 1
-                cur_a = aVals.get()
+            delete += 1
+            a_ind += 1
         elif cur_b < cur_a:
-            while not bVals.empty() and cur_b < cur_a:
-                delete += 1
-                cur_b = bVals.get()
+            delete += 1
+            b_ind += 1
