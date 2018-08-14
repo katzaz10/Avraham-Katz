b49dc67ef6d2bdeeedf83237b2d436ceb600019a
katzaz10 <31491542+katzaz10@users.noreply.github.com>
2018-08-14 15:32:30 -0400

    Update solution.py

--- HackerRankSolutions/Trees/BinarySearchTreeLowestCommonAncestor/solution.py
+++ HackerRankSolutions/Trees/BinarySearchTreeLowestCommonAncestor/solution.py
@@ -4,10 +4,8 @@ def lca(root, v1, v2):
     if v1 > root.info:
         if v2 < root.info:
             return root
-        #v1 and v2 are greater than root, so go right
-        return lca(root.right, v1, v2)
+        return lca(root.right, v1, v2) #v1 and v2 are greater than root, so go right
     #v1 is less than root
     if v2 > root.info:
         return root
-    #v1 and v2 are both less than root, so go left
-    return lca(root.left, v1, v2)
+    return lca(root.left, v1, v2) #v1 and v2 are both less than root, so go left
