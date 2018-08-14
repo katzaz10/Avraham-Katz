1abc650e9eb55046a5c4a095d33894920cfc24d2
katzaz10 <katzaz10@gmail.com>
2018-08-14 19:58:33 -0400

    solved sorted insert

--- HackerRankSolutions/LinkedLists/InsertNodeAtASpecificPositionInALinkedList/solution.py
+++ HackerRankSolutions/LinkedLists/InsertNodeAtASpecificPositionInALinkedList/solution.py
@@ -1,10 +1,10 @@
 def insertNodeAtPosition(head, data, position):
-    ins_node = SinglyLinkedListNode(data)
+    insert = SinglyLinkedListNode(data)
     if head is None and position == 0:
-        return ins_node
+        return insert
     cur_node = head
     for i in range(position - 1):
         cur_node = cur_node.next
-    ins_node.next = cur_node.next
-    cur_node.next = ins_node
+    insert.next = cur_node.next
+    cur_node.next = insert
     return head
