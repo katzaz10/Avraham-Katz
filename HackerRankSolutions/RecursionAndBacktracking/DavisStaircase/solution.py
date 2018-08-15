875ee3563cb01df76d8e1026513a7df5c24847a0
katzaz10 <katzaz10@gmail.com>
2018-08-14 22:26:58 -0400

    touched up david step

--- HackerRankSolutions/RecursionAndBacktracking/DavisStaircase/solution.py
+++ HackerRankSolutions/RecursionAndBacktracking/DavisStaircase/solution.py
@@ -7,7 +7,9 @@ def step_up(cur_step, n, ways_from_step):
         return 0
     if ways_from_step.get(cur_step, None) is not None:
         return ways_from_step[cur_step]
-    ways_from_step[cur_step] = step_up(cur_step + 1, n, ways_from_step) + step_up(cur_step + 2, n, ways_from_step) + step_up(cur_step + 3, n, ways_from_step)
+    ways_from_step[cur_step] = step_up(cur_step + 1, n, ways_from_step) + \
+                               step_up(cur_step + 2, n, ways_from_step) + \
+                               step_up(cur_step + 3, n, ways_from_step)
     return ways_from_step[cur_step]
 
 
