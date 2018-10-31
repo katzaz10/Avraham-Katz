7cb8db551f22fa2de89eadab1016dd6c650ba7fe
katzaz10 <katzaz10@gmail.com>
2018-10-31 14:26:01 -0400

    refactored repo

--- HackerRankSolutions/Arrays/LeftRotation/solution.py
+++
@@ -1,5 +0,0 @@
-def rotLeft(a, d):
-    d = d % len(a)
-    return a[d:] + a[:d]
-
-
--- HackerRankSolutions/LinkedLists/ReverseADoublyLinkedList/solution.py
+++
@@ -1,7 +0,0 @@
-def reverse(head):
-    if not head:
-        return head
-    head.next, head.prev = head.prev, head.next
-    if not head.prev:
-        return head
-    return reverse(head.prev)
